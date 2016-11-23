c434b68e713bd7636df8d9742e5cf17844471c0c
Matthew Lin <Matthew Lin>
2016-11-22 20:34:54 -0500

    Removed certain comments and reordered to follow handlebars format

--- client_side.js
+++ client_side.js
@@ -1,20 +1,19 @@
 $(document).ready(function() {
-    //for materializewaves-effect purple darken-4 btn
     $('select').material_select();
 $("#submit").on("click", function(){
     var isValid = true;
     var firstNameV = $("#fname").val().trim();
     var lastNameV = $("#lname").val().trim();
     var emailV = $("#email").val().trim();
+    var zipCodeV = $("#zipCode").val().trim();
     var interestV = $("#interest").val().trim();
     var passwordV = $("#pass").val().trim();
-    var zipCodeV = $("#zipCode").val().trim();
-    if(!firstNameV || !lastNameV || !emailV || !interestV || !passwordV || !zipCodeV){
+    if(!firstNameV || !lastNameV || !emailV || !zipCodeV || !interestV || !passwordV ){
       isValid = false;
     }
     if(!isValid){
       alert("All fieldss are required");
       return false;
     }
-});//submit on click
-}); //Document READY
+});
+});
