a5d73fbe0d5d92b4d02b301c7be7b9ce48bac2e0
Senthilselva <senthilselvak@gmail.com>
2016-11-28 11:43:57 -0500

    commit to merger

--- controllers/jobs_controllers.js
+++ controllers/jobs_controllers.js
@@ -5,7 +5,6 @@ var router  = express.Router();
 router.get('/', function(req, res) {
 	console.log("jjjjooooobbbbb    "+ req.session.email)
 	var zipCodeVar=req.session.zipCode;
-	var zipCodeVar='08840';
 
 	models.Jobs.findAll({
 		include: [ models.Organization ],
@@ -14,20 +13,6 @@ router.get('/', function(req, res) {
 
 		res.json(data);
 	})
-
-
-
-	// models.User.findOne({
- //    where: {email: req.session.email}
- //  }).then(function(user){
-	// 		console.log(user.zipCode);
- //  			models.Jobs.findAll({
- //    		include: [models.Organizations],
- //  			where: {jobZipCode: user.zipCode}
- //  			}) .then(function(jobAll) {
- //  					res.json(jobAll);
- //  		})
- //  	});
 });
 
 module.exports = router;
